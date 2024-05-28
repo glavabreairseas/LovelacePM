d2927c8650ae049b18e527498eca39abc36cfc9d
Guilhem Lavabre <guilhem.lavabre@airseas.com>
2024-05-28 15:30:53 +0200

    new tasks

---
+++ tasks.py
@@ -0,0 +1,75 @@
+from invoke import task
+
+
+@task()
+def develop(c):
+    c.run("python setup.py develop")
+    c.run("pip install -e .[dev]")
+
+
+@task()
+def install(c):
+    c.run("python setup.py install")
+
+
+@task()
+def test(c):
+    c.run("pytest tests/")
+
+
+@task
+def testall(c):
+    c.run("tox")
+
+
+@task
+def cover(c):
+    c.run(
+        "pytest tests -v --cov-report term --cov-report html:htmlcov --cov-report xml --cov=./src/"
+    )
+    c.run("coverage report")
+    c.run("coverage xml")
+
+
+@task
+def quality(c):
+    c.run("black --check src/ tests/")
+    c.run("flake8 src/ tests/")
+
+
+@task
+def format(c):
+    c.run("black src/ tests/")
+
+
+@task
+def clean(c):
+    c.run("rm -rf target/* coverage* htmlcov/")
+
+
+@task
+def diagram(c):
+    import plantuml
+    from py2puml.py2puml import py2puml
+
+    # writes the PlantUML content in a file
+    with open("docs/class_diagram.plantuml", "w", encoding="utf8") as puml_file:
+        puml_file.writelines(py2puml("src/LovelacePM", "LovelacePM"))
+
+    p = plantuml.PlantUML("http://www.plantuml.com/plantuml/img/")
+    p.processes_file("docs/class_diagram.plantuml", "docs/class_diagram.png")
+
+
+@task
+def doc(c, path=r"./docs"):
+    import LovelacePM._version as version
+
+    c.run("pip install pdoc")
+    c.run(f'pdoc src/LovelacePM -o "{path}/latest"')
+    c.run(f'pdoc src/LovelacePM -o "{path}/{version.version}"')
+
+@task
+def deploy(c):
+    c.run("python setup.py clean")
+    c.run("python setup.py sdist bdist_wheel")
+    c.run("twine upload --repository-url http://192.168.2.204 -u . -p . dist/*")
