13e05ae68417d1828b57d3621df47d82f44487df
Guilhem Lavabre <guilhem.lavabre@airseas.com>
2024-05-28 13:11:51 +0200

    Quality of utils + added position_airfoil routine to fix bugs with twisted, dihedral and swept geometries

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setup(
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
     ],
-    install_requires=['numpy', 'func-timeout', 'scipy', 'matplotlib', 'cloudpickle', 'PyQt5', 'pyqtgraph', 'PyOpenGL', 'LoveUpdate'],
-    python_requires='>=3.6',
+    install_requires=['numpy', 'numpy-quaternion', 'func-timeout', 'scipy', 'matplotlib', 'cloudpickle', 'pyqtgraph', 'PyOpenGL', 'LoveUpdate'],
+    python_requires='>=3.11',
 )
 
 try:
