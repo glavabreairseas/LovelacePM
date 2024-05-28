af48a01ed87f06f7e422eb4c76ce72b1b0734395
Guilhem Lavabre <guilhem.lavabre@airseas.com>
2024-05-28 12:57:33 +0200

    Quality of mutiprocess_guard

--- src/multiprocess_guard.py
+++ src/multiprocess_guard.py
@@ -1,4 +1,9 @@
 import multiprocessing as mp
 
-def multiprocess_guard(): #return False if code is being executed from LPM's child processes
-    return mp.current_process().name!='LPM_child' #built to be implemented as a more proper if __name__=='__main__' guard for Windows multiprocessing
+
+def multiprocess_guard():
+    # return False if code is being executed from LPM's child processes
+    return (
+        mp.current_process().name != "LPM_child"
+    )  # built to be implemented as a more proper if __name__=='__main__' guard
+    # for Windows multiprocessing
